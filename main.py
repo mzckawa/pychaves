9f7ae30d7382f429846d0561f2b2fc9e787865f6
Mel Zion Cordeiro <mzc@WINDOWS.CIN.UFPE.BR>
2025-07-31 16:43:59 -0300

    trabalhei mais alguns comandos, como o de alteração de opacidade de imagem, mas, agora, vou passar a testar comandos em um arquivo de teste, em vez de no main

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ screen = pygame.display.set_mode((800, 640))
 
 chaves_img = pygame.image.load('chaves_1.jpg').convert()
 chaves_img = pygame.transform.scale(chaves_img, (chaves_img.get_width() * 3, chaves_img.get_height() * 2))
-chaves_img = pygame.transform.flip(chaves_img, True, False)
+chaves_img = pygame.transform.flip(chaves_img, False, True)
+chaves_img.set_colorkey((0, 0, 0))
 
 running = True
 x = 0 
@@ -17,11 +18,10 @@ while running:
 
     screen.fill((255, 205, 255))
 
-    screen.blit(chaves_img, (x, 2))
+    chaves_img.set_alpha(max(0, 255))
+    screen.blit(chaves_img, (x, 30))
 
-    x += 10 * delta_time
-
-    
+    # x += 10 * delta_time
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
