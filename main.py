8570b9fe3315d63c996dbdf0811469fcdd0aff0b
Mel Zion Cordeiro <mzc@windows.cin.ufpe.br>
2025-07-31 15:13:45 -0300

    testei comandos do pygame

--- main.py
+++ main.py
@@ -2,13 +2,33 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 640))
+screen = pygame.display.set_mode((800, 640))
+
+chaves_img = pygame.image.load('chaves_1.jpg').convert()
+chaves_img = pygame.transform.scale(chaves_img, (chaves_img.get_width() * 3, chaves_img.get_height() * 2))
+chaves_img = pygame.transform.flip(chaves_img, True, False)
 
 running = True
+x = 0 
+clock = pygame.time.Clock()
+delta_time = 0.1
 
 while running:
+
+    screen.fill((255, 205, 255))
+
+    screen.blit(chaves_img, (x, 2))
+
+    x += 10 * delta_time
+
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    pygame.time.wait(1)
+    pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
