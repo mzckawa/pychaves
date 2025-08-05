e893c47ddf22fe79262f0e891f4c4599655a6c7a
mzckawa <mzc@cin.ufpe.br>
2025-08-05 16:48:02 -0300

    testando comandos até a aula 6 do curso

---
+++ main.py
@@ -0,0 +1,58 @@
+import pygame
+import math
+
+pygame.init()
+
+#define tempo do jogo
+clock = pygame.time.Clock()
+FPS = 60
+
+#dimencionamento da tela
+comprimeto_tela = 860
+largura_tela = 640
+
+#criando a tela
+tela = pygame.display.set_mode((comprimeto_tela, largura_tela))
+pygame.display.set_caption("pychaves")
+
+#receber a imagem dos arquivos
+imagem_cenario = pygame.image.load("cenario_jogo.jpg").convert()
+#convercao da imagem
+imagem_cenario = pygame.transform.scale(imagem_cenario, (2*comprimeto_tela/3, largura_tela))
+imagem_comprimento = imagem_cenario.get_width()
+
+
+#variaveis do jogo
+scroll = 0
+partes = math.ceil(comprimeto_tela/imagem_comprimento) + 2
+print(partes)
+#loop do jogo
+
+velocidade = 3
+run = True
+while run:
+
+    clock.tick(FPS)
+
+    #cenario infinito
+    for i in range (0, partes):
+        tela.blit(imagem_cenario, (i * imagem_comprimento + scroll, 0))
+    scroll -= velocidade
+
+    # resetando scroll 
+    if abs(scroll) > imagem_comprimento:
+        scroll = 0
+
+    #receber evento
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    if velocidade == 8:
+        velocidade  = 8
+    else: 
+        velocidade += 0.0005
+
+    pygame.display.update()
+
+pygame.quit()
