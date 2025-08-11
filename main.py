74169f36f3bc5de8d7be408097d7f746adadce1c
Jgpor7 <jgpor@cin.ufpe.br>
2025-08-11 11:12:17 -0300

    adicao do sistema de morte, bem como da barreira de morte

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ print(partes)
 chaves = Jogador(400, 400, 100, 100)
 obstaculos = [
     pygame.Rect(0 , 300, 860, 10),
-    pygame.Rect(-100 , 0, 1, 640),
     pygame.Rect(860 , 0, 1, 640),
     pygame.Rect(0 , 640, 860, 1)
 ]
@@ -51,67 +50,81 @@ chaves.get_velocidade_correnteza(velocidade)
 
 #loop do jogo
 run = True
+morte = False
 while run:
+    morte = False
+    velocidade = 3
+    scroll = 0
+    while not morte:
 
-    # rodando o menu
-    menu_do_jogo.rodar_menu()
+        # rodando o menu
+        menu_do_jogo.rodar_menu()
 
-    # verificando se eu quero fechar a tela do jogo durante o menu
-    if menu_do_jogo.sair_do_jogo == False: # se eu não quero fechar, vou entrar em alguma das 2 telas do jogo: o jogo em si e os créditos
+        # verificando se eu quero fechar a tela do jogo durante o menu
+        if menu_do_jogo.sair_do_jogo == False: # se eu não quero fechar, vou entrar em alguma das 2 telas do jogo: o jogo em si e os créditos
 
-        # verificando se o jogador apertou o botão START
-        if menu_do_jogo.selected_index == 0:
+            # verificando se o jogador apertou o botão START
+            if menu_do_jogo.selected_index == 0:
 
-            clock.tick(FPS)
-            chaves.get_velocidade_correnteza(velocidade)
-            #cenario infinito
-            for i in range (0, partes):
-                tela.blit(imagem_cenario, (i * imagem_comprimento + scroll, 0))
-            scroll -= velocidade
+                clock.tick(FPS)
+                chaves.get_velocidade_correnteza(velocidade)
+                #cenario infinito
+                for i in range (0, partes):
+                    tela.blit(imagem_cenario, (i * imagem_comprimento + scroll, 0))
+                scroll -= velocidade
 
-            teclas = pygame.key.get_pressed()
-            chaves.mover(teclas, obstaculos)
+                teclas = pygame.key.get_pressed()
+                chaves.mover(teclas, obstaculos)
 
-            # resetando scroll 
-            if abs(scroll) > imagem_comprimento:
-                scroll = 0
+                # resetando scroll 
+                if abs(scroll) > imagem_comprimento:
+                    scroll = 0
 
-            #receber evento
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                #receber evento
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        morte = True
+                        run = False
+
+                if velocidade >= 6:
+                    velocidade  = 6
+                else: 
+                    velocidade += 0.001
+
+                chaves.get_velocidade_correnteza(velocidade)
+
+                if chaves.get_colisao_barreira_morte(): #codicao de morte do chaves obs: tem que resetar o coletaveis se derrota por isso
+                    morte = True
+                    tela.fill((0,0,0))
+                    menu_do_jogo.selected_index = 0
+                    menu_do_jogo.running = True
+
+                chaves.desenhar(tela)
+
+                pygame.display.update()
+
+            # verificando se o jogador apertou o botão credits
+            elif menu_do_jogo.selected_index == 1:
+
+                clock.tick(FPS)
+
+                creditos_do_jogo.rodar_tela_creditos()
+                
+                if creditos_do_jogo.fechar_tela == True:
+                    
                     run = False
 
-            if velocidade >= 6:
-                velocidade  = 6
-            else: 
-                velocidade += 0.001
+                elif creditos_do_jogo.fechar_tela == False:
 
-            chaves.get_velocidade_correnteza(velocidade)
+                    menu_do_jogo.running = True
+                    creditos_do_jogo.running = True
 
-            chaves.desenhar(tela)
+        # verificando se eu quero fechar a tela do jogo durante o menu
+        else:
 
-            pygame.display.update()
-
-        # verificando se o jogador apertou o botão credits
-        elif menu_do_jogo.selected_index == 1:
-
-            clock.tick(FPS)
-
-            creditos_do_jogo.rodar_tela_creditos()
+            morte = True
             
-            if creditos_do_jogo.fechar_tela == True:
-                
-                run = False
-
-            elif creditos_do_jogo.fechar_tela == False:
-
-                menu_do_jogo.running = True
-                creditos_do_jogo.running = True
-
-    # verificando se eu quero fechar a tela do jogo durante o menu
-    else:
-        
-        # mudando o valor da variável que controla o loop
-        run = False
+            # mudando o valor da variável que controla o loop
+            run = False
 
 pygame.quit()
